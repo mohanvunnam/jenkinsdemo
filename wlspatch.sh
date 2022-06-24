5435c28856c5a65338ba1930160db75ee54f0aa5
mohanvunnam <mohan.vunnam@gmail.com>
2022-06-24 09:58:47 -0400

    WblogicPatchParallel.xml  24Jun2022_095847

--- wlspatch.sh
+++ wlspatch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Hello from $HOSTNAME"
 export DIR=/home/mvunnam
-export WL_VERSION=`ls $DIR/Jenkins/jenkinsdemo | grep -i wlspatch | awk -F\- '{print $NF}'`
+export WL_VERSION=`ls /tmp/ | grep -i wlspatch | awk -F\- '{print $NF}'`
 
 echo "$WL_VERSION"
 
-echo "$WL_VERSION at `date`" >> /tmp/testwls.txt
+echo "$WL_VERSION and $DIR at `date`" >> /tmp/testwls.txt
